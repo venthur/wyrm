1b829588a0e47666fa014df8a283a3b409c79404
Bastian Venthur <venthur@debian.org>
2013-07-10 10:36:53 +0200

    Added script for unittest, coverage and pyflakes.

---
+++ testall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+pprint() {
+    echo ============================================================
+    echo $@
+    echo ============================================================
+}
+
+pprint Running Pyflakes
+pyflakes .
+
+pprint Running Unittests
+coverage run --source=wyrm --module unittest discover test
+
+pprint Coverage Report
+coverage report
