3c1221ee7cfb8b8af4f058f8f40bceb1241a8e68
Bastian Venthur <venthur@debian.org>
2013-11-26 12:23:14 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ setup(
     name='Wyrm',
     version=wyrm.__version__,
     description='Toolbox for Brain Computer Interfacing Experiments.',
+    long_description='A Python toolbox of on-line BCI experiments and off-line BCI data analysis.',
     author='Bastian Venthur',
     author_email='bastian.venthur@tu-berlin.de',
     url='http://github.com/venthur/wyrm/',
+    download_url='http://github.com/venthur/wyrm/',
+    license='GPL2',
+    platform='any',
     packages=['wyrm'],
 )
