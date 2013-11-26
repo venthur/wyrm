9fd40d7848f340b4ca94955358887e77101e031b
Bastian Venthur <venthur@debian.org>
2013-11-26 12:35:48 +0100

    Added more trove classifiers.

--- setup.py
+++ setup.py
@@ -18,4 +18,22 @@ setup(
     license='GPL2',
     platforms='any',
     packages=['wyrm'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Software Development :: Libraries',
+        ]
 )
