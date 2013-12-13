729e97c0969a52cffab9560fb0fb12ca6f434ea9
hendrikheller <hendrik.heller@campus.tu-berlin.de>
2013-12-13 13:46:23 +0100

    moved methods for testdata creation to new file 'testdata.py'. plot_tenten() slicker annotation of channels.

--- wyrm/plot.py
+++ wyrm/plot.py
@@ -9,16 +9,18 @@ This module contains various plotting methods.
 from __future__ import division
 
 import numpy as np
-import random as rnd
+from scipy import interpolate
 import inspect
+
 from matplotlib import colorbar
 from matplotlib import colors
 from matplotlib import pyplot as plt
-import matplotlib.gridspec as gridspec
+from matplotlib import gridspec as gridspec
 from matplotlib.path import Path
-import matplotlib.patches as patches
+from matplotlib import patches as patches
+
 from wyrm.types import Data
-from scipy import interpolate
+
 import tentensystem as tts
 
 
@@ -116,102 +118,6 @@ def interpolate_2d(x, y, z):
     f = interpolate.LinearNDInterpolator(zip(x, y), z)
     Z = f(X, Y)
     return X, Y, Z
-
-
-# automated creation of test data
-def _create_channel(steps = 100):
-    steps = float(steps)
-    a = -5
-    b = -2
-    rnd_fac1 = rnd.randrange(25, 200) / 100.
-    rnd_fac2 = rnd.randrange(-200, 200) / 100.
-    rnd_fac3 = rnd.randrange(-100, 100) / 100.
-    range_x = np.arange(a, b, np.absolute(a-b)/steps)
-    range_y = np.zeros(steps)
-    
-    cnt = 0
-    for i in range_x:
-        range_y[cnt] = (i**3 * np.cos(i - rnd_fac3) / 20) * rnd_fac1 - rnd_fac2
-        cnt += 1
-    
-    return range_y
-
-
-# test function: (x^3 * cos(x)) / 20, x in [-5, -2] (with variations)
-def create_data_ti(channel_count = 2, steps = 100):
-    
-    data = np.zeros([steps, channel_count])
-    channels = []
-    for i in range(channel_count):
-        data[:,i] = _create_channel(steps)
-        channels.append('ch' + str(i))
-        
-    axes = [np.arange(0,steps*10, 10), channels]
-    names = ["time", "channel"]
-    units = ["ms", "channel_stuff"]
-    dat = Data(data, axes, names, units)
-    return (dat)
-
-def create_data_all():
-    d = create_data_ti(channel_count=142)
-    d.axes[1] = np.array(tts.channels.keys())
-    return d
-
-def create_data_scalp(t=0):
-    d = create_data_ti(channel_count=142)
-    chans = np.array(tts.channels.keys())
-    return (d.data[t], chans)
-
-def create_data_some():
-    d = create_data_ti(channel_count=25)
-    d.axes[1] = np.array(['Fpz','AF7','AF8','FFC9','FFC10','AF5','AF6','FFC5','FFC6','T7','T8','C3','C4','Cz','TP7','TP8','P1','P2','P7','P8','PO5','PO6','PO7','PO8','Oz'])
-    return d
-
-
-def create_epoched_data_ti(epoch_count=4, channel_count=2, steps=100):
-    data = np.zeros([epoch_count, steps, channel_count])
-    for i in range(epoch_count):
-        for o in range(channel_count):
-            data[i,:,o] = _create_channel(steps)
-            
-    # create the channel labels
-    channels = []
-    for i in range(channel_count):
-        channels.append('ch' + str(i))
-        
-    # create the class labels
-    classes = []
-    for i in range(epoch_count):
-        classes.append('class' + str(i%2))
-        
-    axes = [classes, np.arange(0,steps*10, 10), channels]
-    names = ["class", "time", "channel"]
-    units = ["class_stuff", "ms", "channel_stuff"]
-    dat = Data(data, axes, names, units)
-    return(dat)
-
-def create_tenten_data(channel_count=21, steps=100):
-    data = np.zeros([steps, channel_count])
-    channels = []
-    chan_names = ['A1', 'A2', 'C3', 'C4', 'Cz', 'Fp1', 'Fp2', 'F3', 'F4', 'F7', 'F8', 'Fz', 'O1', 'O2', 'P3', 'P4', 'Pz', 'T3', 'T4', 'T5', 'T6']
-         
-    for i in range(channel_count):
-        data[:,i] = _create_channel(steps)
-        
-    if channel_count <= len(chan_names):
-        channels = chan_names[:channel_count]
-    else:
-        channels = chan_names
-        i = len(chan_names)
-        while (i < channel_count):
-            channels.append('ch' + str(i))
-            i = i+1
-
-    axes = [np.arange(0,steps*10, 10), channels]
-    names = ["time", "channel"]
-    units = ["ms", "channel_stuff"]
-    dat = Data(data, axes, names, units)
-    return (dat)
 
 
 # some custom colormaps
@@ -326,7 +232,7 @@ def plot_epoched_timeinterval(data, highlights=None, legend=True, show=True, sav
     
     
 # plots all recognized channels of the system according to their position on the scalp in a grid.
-def plot_tenten(data, highlights=None, legend=True, show=True, save=False, save_name='system_plot', save_path=None):
+def plot_tenten(data, highlights=None, legend=False, show=True, save=False, save_name='system_plot', save_path=None):
 
     # this dictionary determines which y-position corresponds with which row in the grid
     ordering = {4.0  : 0,
@@ -528,14 +434,14 @@ def plot_tenten(data, highlights=None, legend=True, show=True, save=False, save_
         if len(l) > 0:
             for i in range(len(l)):
                 col_pos = int(i + ((columns-len(l)) - np.ceil((columns-len(l))/2.)))
-                _subplot_timeinterval(data, gs[row, col_pos], epoch=-1, highlights=highlights, legend=True, channel=l[i][2])
+                _subplot_timeinterval(data, gs[row, col_pos], epoch=-1, highlights=highlights, legend=legend, channel=l[i][2])
                 
                 # hide the axes
                 plt.gca().get_xaxis().set_visible(False)
                 plt.gca().get_yaxis().set_visible(False)
                 
                 # at this moment just to show what's what
-                #plt.gca().annotate(l[i][0], (20, 20), xycoords='axes pixels')
+                plt.gca().annotate(l[i][0], (0.05, 0.80), xycoords='axes fraction')
             row=row+1
     
     # adjust the spacing
