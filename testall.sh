a58d3b942458c52e627e2478b323183be28f6b16
Bastian Venthur <venthur@debian.org>
2014-03-18 11:07:44 +0100

    Automatically detect presence of coverage or python-coverage.

--- testall.sh
+++ testall.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 
+
+if command -v coverage >/dev/null
+then 
+    COVERAGE=ccoverage
+elif command -v python-coverage >/dev/null
+then
+    COVERAGE=python-coverage
+else
+    echo "No Python Coverage found, aborting."
+    exit 1
+fi
+
 pprint() {
     echo
     echo ============================================================
@@ -13,7 +25,8 @@ pprint Running Pyflakes
 PYFLAKES_NODOCTEST=1 pyflakes .
 
 pprint Running Unittests
-coverage run --branch --source=wyrm --module unittest discover test || exit
+$COVERAGE run --branch --source=wyrm --module unittest discover test
 
 pprint Coverage Report
-coverage report
+$COVERAGE report
+
