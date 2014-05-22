f56bbfe97068e2c702ca82a2facac3bf72a7641a
Bastian Venthur <venthur@debian.org>
2014-05-22 11:57:42 +0200

    Some polish.

--- examples/online_experiment.py
+++ examples/online_experiment.py
@@ -8,7 +8,6 @@ import time
 import logging
 
 import numpy as np
-from scipy.io import loadmat
 
 # change this to the path of mushu if you don't have it in your
 # PYTHONPATH already
@@ -16,7 +15,7 @@ sys.path.append('../../mushu')
 sys.path.append('../')
 
 import libmushu
-from wyrm.types import Data, RingBuffer, BlockBuffer
+from wyrm.types import RingBuffer, BlockBuffer
 import wyrm.processing as proc
 from wyrm import io
 
@@ -55,64 +54,6 @@ MARKER_DEF_TEST = {i : [i] for i in STIMULUS_CODE.values()}
 SEG_IVAL = [0, 600]
 
 
-def load_dataset(filename):
-    # load the matlab data
-    data_mat = loadmat(filename)
-    # load the channel names (the same for all datasets
-    with open(CHANNEL_DATA) as fh:
-        data = fh.read()
-    channels = []
-    for line in data.splitlines():
-        if line:
-            chan = line.split()[-1]
-            chan = chan.replace('.', '')
-            channels.append(chan)
-    # The signal is recorded with 64 channels, bandpass filtered 0.1-60Hz and digitized at 240Hz. The format is Character Epoch x Samples x Channels
-    data = data_mat['Signal']
-    data = data.astype('double')
-    # For each sample: 1 if a row/colum was flashed, 0 otherwise
-    flashing = data_mat['Flashing'].reshape(-1)
-    #flashing = np.flatnonzero((np.diff(a) == 1)) + 1
-    tmp = []
-    for i, _ in enumerate(flashing):
-        if i == 0:
-            tmp.append(flashing[i])
-            continue
-        if flashing[i] == flashing[i-1] == 1:
-            tmp.append(0)
-            continue
-        tmp.append(flashing[i])
-    flashing = np.array(tmp)
-    # For each sample: 0 when no row/colum was intensified, 1..6 for intensified columns, 7..12 for intensified rows
-    stimulus_code = data_mat['StimulusCode'].reshape(-1)
-    stimulus_code = stimulus_code[flashing == 1]
-    # 0 if no row/col was intensified or the intensified did not contain the target character, 1 otherwise
-    stimulus_type = data_mat.get('StimulusType', np.array([])).reshape(-1)
-    # The target characters
-    target_chars = data_mat.get('TargetChar', np.array([])).reshape(-1)
-    fs = 240
-    data = data.reshape(-1, 64)
-    timeaxis = np.linspace(0, data.shape[0] / fs * 1000, data.shape[0], endpoint=False)
-    dat = Data(data=data, axes=[timeaxis, channels], names=['time', 'channel'], units=['ms', '#'])
-    dat.fs = fs
-    # preparing the markers
-    target_mask = np.logical_and((flashing == 1), (stimulus_type == 1)) if len(stimulus_type) > 0 else []
-    nontarget_mask = np.logical_and((flashing == 1), (stimulus_type == 0)) if len(stimulus_type) > 0 else []
-    flashing = (flashing == 1)
-    flashing = [[i, 'flashing'] for i in timeaxis[flashing]]
-    targets = [[i, 'target'] for i in timeaxis[target_mask]]
-    nontargets = [[i, 'nontarget'] for i in timeaxis[nontarget_mask]]
-    stimulus_code = zip([t for t, _ in flashing], [STIMULUS_CODE[i] for i in stimulus_code])
-    markers = []
-    markers.extend(targets)
-    markers.extend(nontargets)
-    markers.extend(stimulus_code)
-    markers.sort(key=lambda x: x[0])
-    dat.markers = markers[:]
-    dat.stimulus_code = stimulus_code
-    return dat
-
-
 def online_experiment(amp, clf):
     amp_fs = amp.get_sampling_frequency()
     amp_channels = amp.get_channels()
@@ -142,9 +83,6 @@ def online_experiment(amp, clf):
         # convert to cnt
         cnt = io.convert_mushu_data(data, markers, amp_fs, amp_channels)
 
-        #print cnt.axes[0]
-        #print cnt.markers
-
         # enter the block buffer
         buf.append(cnt)
         cnt = buf.get()
@@ -167,10 +105,6 @@ def online_experiment(amp, clf):
         fv = proc.create_feature_vectors(epo)
         logger.debug(markers_processed)
 
-        # TODO compare all fv.axes[0] with markers from file to see if
-        # they are the same
-        #print fv.axes[0]
-        #print fv
         lda_out = proc.lda_apply(fv, clf)
         markers = [fv.class_names[cls_idx] for cls_idx in fv.axes[0]]
         result = zip(markers, lda_out)
@@ -199,8 +133,8 @@ def online_experiment(amp, clf):
 
     amp.stop()
 
+
 def train(filename):
-    #dat = load_dataset(filename)
     dat = io.load_bcicomp3_ds2(filename)
     b, a = proc.signal.butter(5, [10 / (dat.fs / 2)], btype='low')
     dat = proc.lfilter(dat, b, a)
@@ -216,7 +150,6 @@ if __name__ == '__main__':
     clf = train(TRAIN_DATA)
 
     logger.debug('Starting Online experiment...')
-    #cnt = load_dataset(TEST_DATA)
     cnt = io.load_bcicomp3_ds2(TEST_DATA)
     amp = libmushu.get_amp('replayamp')
     amp.configure(data=cnt.data, marker=cnt.markers, channels=cnt.axes[-1], fs=cnt.fs)
